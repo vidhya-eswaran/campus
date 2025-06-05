19e8b6d0bd7edbacaee1fe1b7e3dcb954ae8eb8f
vidhya-eswaran <vidhya.eswaran@eucto.com>
2025-06-05 15:20:11 +0530

    web file changes project

--- routes/web.php
+++ routes/web.php
@@ -19,52 +19,52 @@ use App\Http\Controllers\DonationController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/donate', [DonationController::class, 'showForm']);
-Route::post('/donate/step1', [DonationController::class, 'storeStep1'])->name('donate.step1');
-Route::post('/donate/step2', [DonationController::class, 'storeStep2'])->name('donate.step2');
-Route::post('/donate/submit', [DonationController::class, 'storeFinal'])->name('donate.submit');
-Route::get('/thank-you', [DonationController::class, 'thankYou'])->name('thank.you');
+// Route::get('/donate', [DonationController::class, 'showForm']);
+// Route::post('/donate/step1', [DonationController::class, 'storeStep1'])->name('donate.step1');
+// Route::post('/donate/step2', [DonationController::class, 'storeStep2'])->name('donate.step2');
+// Route::post('/donate/submit', [DonationController::class, 'storeFinal'])->name('donate.submit');
+// Route::get('/thank-you', [DonationController::class, 'thankYou'])->name('thank.you');
 
-Route::post('/add-student', [App\Http\Controllers\API\StudentController::class, 'index']);
-Route::get('/dashboard', [App\Http\Controllers\API\dashboardController::class, 'countstudent']);
+// Route::post('/add-student', [App\Http\Controllers\API\StudentController::class, 'index']);
+// Route::get('/dashboard', [App\Http\Controllers\API\dashboardController::class, 'countstudent']);
 
-Route::get('/intiate-payment', [PaymentsController::class, 'intiatePayment']);
-Route::post('/process-data', [PaymentsController::class, 'processData']);
-Route::post('/redirect', [PaymentsController::class, 'processRetrunResponse']);
-Route::resource('admission',AdmissionController::class);
-Route::get('/admission-test', [AdmissionController::class, 'indexdemo']);
-Route::post('/admission-redirect', [AdmissionController::class, 'admissionRetrunResponse']);
-Route::get('/admission-data', [AdmissionController::class, 'Data']);
-Route::get('/admission-transection-data', [AdmissionController::class, 'Data_1']);
-Route::get('/admission-offline', [AdmissionController::class, 'offline_store']);
+// Route::get('/intiate-payment', [PaymentsController::class, 'intiatePayment']);
+// Route::post('/process-data', [PaymentsController::class, 'processData']);
+// Route::post('/redirect', [PaymentsController::class, 'processRetrunResponse']);
+// Route::resource('admission',AdmissionController::class);
+// Route::get('/admission-test', [AdmissionController::class, 'indexdemo']);
+// Route::post('/admission-redirect', [AdmissionController::class, 'admissionRetrunResponse']);
+// Route::get('/admission-data', [AdmissionController::class, 'Data']);
+// Route::get('/admission-transection-data', [AdmissionController::class, 'Data_1']);
+// Route::get('/admission-offline', [AdmissionController::class, 'offline_store']);
 //Route::post('/offline-store', [AdmissionController::class,'offline_store'])->name('admission.offline_store');
 
 
 
-Route::get('/donate', function () {
-    return view('donation.donate');
-})->name('donate');
-Route::post('/donate/submit', [DonationController::class, 'submitDonation'])->name('submitDonation');
+// Route::get('/donate', function () {
+//     return view('donation.donate');
+// })->name('donate');
+//Route::post('/donate/submit', [DonationController::class, 'submitDonation'])->name('submitDonation');
 Route::get('/thank-you', function () {
     return view('thank-you');
 })->name('thank.you');
-Route::post('/donation-redirect', [DonationController::class, 'donationRedirect'])->name('donation.redirect');
+//Route::post('/donation-redirect', [DonationController::class, 'donationRedirect'])->name('donation.redirect');
 
 
 
 // Route::get('/admission/addedit', 'AdmissionController@addedit')->name('admission.addedit');
 
 //Email
-Route::get('/admission-view/{id}', [AdmissionController::class, 'view'])->name('admission.view_admission');
+//Route::get('/admission-view/{id}', [AdmissionController::class, 'view'])->name('admission.view_admission');
 //Email
 
 // Route::get('/', function () {
 //     dd("Ffffff");
 // });
-Route::get('/offline-store', [AdmissionController::class,'offline_store'])->name('admission.offline_store');
+//Route::get('/offline-store', [AdmissionController::class,'offline_store'])->name('admission.offline_store');
 
 //Route::get('/admission-view/{id}', 'App\Http\Controllers\AdmissionController@view')->name('admission.view_admission');
-Route::get('/documents/{id}',[AdmissionController::class,'documents'])->name('admission.documents');
+//Route::get('/documents/{id}',[AdmissionController::class,'documents'])->name('admission.documents');
 
 // Route::get('/admission-view/{id}', function () {
 // return view('admission.view_admission');
@@ -72,15 +72,15 @@ Route::get('/documents/{id}',[AdmissionController::class,'documents'])->name('ad
 // });
 
 
-Route::get('/donations', [DonationController::class, 'index'])->name('donations.index');
-Route::get('/donation-details/{id}', [DonationController::class, 'show'])->name('donation.details');
-Route::post('/donation-confirm', [DonationController::class, 'confirm'])->name('donation.confirm');
-// Route::post('/donation-payment', [DonationController::class, 'processPayment'])->name('donation.payment');
-Route::get('/donation-success/{transaction_id}', [DonationController::class, 'paymentSuccess'])->name('donations.success');
-// Route::post('/donation-payment', [DonationController::class, 'processPayment'])->name('donation.payment');
-Route::post('/payment-response', [DonationController::class, 'paymentResponse'])->name('payment.response');
-Route::post('/donation-redirect', [DonationController::class, 'donationRedirect'])->name('donation.redirect');
-// Route::get('/donation-payment', function () {
-//     return view('donation_payment'); // Your Blade file
-// })->name('donation.payment');
-Route::post('/process-payment', [DonationController::class, 'processPayment'])->name('donation.payment');
+// Route::get('/donations', [DonationController::class, 'index'])->name('donations.index');
+// Route::get('/donation-details/{id}', [DonationController::class, 'show'])->name('donation.details');
+// Route::post('/donation-confirm', [DonationController::class, 'confirm'])->name('donation.confirm');
+// // Route::post('/donation-payment', [DonationController::class, 'processPayment'])->name('donation.payment');
+// Route::get('/donation-success/{transaction_id}', [DonationController::class, 'paymentSuccess'])->name('donations.success');
+// // Route::post('/donation-payment', [DonationController::class, 'processPayment'])->name('donation.payment');
+// Route::post('/payment-response', [DonationController::class, 'paymentResponse'])->name('payment.response');
+// Route::post('/donation-redirect', [DonationController::class, 'donationRedirect'])->name('donation.redirect');
+// // Route::get('/donation-payment', function () {
+// //     return view('donation_payment'); // Your Blade file
+// // })->name('donation.payment');
+// Route::post('/process-payment', [DonationController::class, 'processPayment'])->name('donation.payment');
