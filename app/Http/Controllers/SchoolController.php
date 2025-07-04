02ca380da2a7802366d90fcc1262917d627a5793
vidhya-eswaran <vidhya.eswaran@eucto.com>
2025-07-04 10:53:35 +0530

    api file

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -73,12 +73,16 @@ class SchoolController extends Controller
             return response()->json(['error' => 'schema.sql file not found.'], 500);
         }
 
-        $mysqlPath = '/usr/bin/mysql';
+        //$mysqlPath = '/usr/bin/mysql';
+        $mysqlPath = 'C:\\xampp\\mysql\\bin\\mysql.exe';
         $dbUser = 'root';
         $dbPass = ''; // leave blank if no password
-        $schemaPath = '/var/www/html/schema.sql';
+        //$schemaPath = '/var/www/html/schema.sql';
+        $schemaPath = base_path('schema.sql');
 
-        $command = "sudo $mysqlPath -u $dbUser " . ($dbPass ? "-p$dbPass " : "") . "$dbName < \"$schemaPath\"";
+        //$command = "sudo $mysqlPath -u $dbUser " . ($dbPass ? "-p$dbPass " : "") . "$dbName < \"$schemaPath\"";
+        $command = "\"$mysqlPath\" -u $dbUser " . ($dbPass ? "-p$dbPass " : "") . "$dbName < \"$schemaPath\"";
+
 
         // Capture stderr too
         $output = [];
