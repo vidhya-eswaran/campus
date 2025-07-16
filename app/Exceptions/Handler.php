ed5b3f4d2e7a098d17601763eda76dcba1f954a7
vidhya-eswaran <vidhya.eswaran@eucto.com>
2025-07-16 15:04:21 +0530

    middleware

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -33,5 +35,14 @@ class Handler extends ExceptionHandler
     public function register()
     {
         //
+    }
+
+    public function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest(route('login')); // This line is the one causing the issue
     }
 }
