22aa80d75af96a9caa70245fa5665de6c61268c5
vidhya-eswaran <vidhya.eswaran@eucto.com>
2025-08-05 19:00:02 +0530

    old push fila

--- app/Http/Controllers/PushNotificationController.php
+++ app/Http/Controllers/PushNotificationController.php
@@ -13,22 +13,34 @@ class PushNotificationController extends Controller
         $title = $request->input('title');
         $body = $request->input('body');
 
-        $factory = (new Factory)->withServiceAccount(storage_path('app/firebase/firebase_credentials.json'));
-        $messaging = $factory->createMessaging();
+        // $factory = (new Factory)->withServiceAccount(storage_path('app/firebase/firebase_credentials.json'));
+        // $messaging = $factory->createMessaging();
 
-        $message = [
-            'token' => $deviceToken,
-            'notification' => [
-                'title' => $title,
-                'body' => $body,
-            ],
-            'data' => [
-                'custom_key' => 'custom_value'
-            ]
-        ];
+        // $message = [
+        //     'token' => $deviceToken,
+        //     'notification' => [
+        //         'title' => $title,
+        //         'body' => $body,
+        //     ],
+        //     'data' => [
+        //         'custom_key' => 'custom_value'
+        //     ]
+        // ];
 
         try {
-            $messaging->send($message);
+
+            $response = Http::post('https://exp.host/--/api/v2/push/send', [
+                'to' => $deviceToken, // ExponentPushToken[xxx]
+                'title' => $title,
+                'body' => $body,
+                'data' => [
+                    'custom_key' => 'custom_value',
+                ],
+            ]);
+
+            return $response->json();
+
+            //$messaging->send($message);
             return response()->json(['success' => true, 'message' => 'Notification sent']);
         } catch (\Throwable $e) {
             return response()->json(['success' => false, 'error' => $e->getMessage()]);
