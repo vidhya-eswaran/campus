a2d5ee5d7e31679c6cca7c8cf31528a778f9d6b3
vidhya-eswaran <vidhya.eswaran@eucto.com>
2025-08-06 13:03:43 +0530

    template changes

--- app/Http/Controllers/API/StudentAttendanceController.php
+++ app/Http/Controllers/API/StudentAttendanceController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
+use App\Models\User;
 use App\Models\StudentAttendance;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
